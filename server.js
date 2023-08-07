bcc01ee79bfb0945383690e94878902034212e61
Simonas Tamkevicius <simonas.ta@icloud.com>
2023-08-07 12:20:25 -0400

    added edit product functionality and made numerous other changes

--- server.js
+++ server.js
@@ -6,13 +6,13 @@ import {
   S3Client,
   PutObjectCommand,
   GetObjectCommand,
+  DeleteObjectCommand,
 } from "@aws-sdk/client-s3";
 import dotenv from "dotenv";
 import crypto from "crypto";
 import bcrypt from "bcrypt";
 import cors from "cors";
-import { log } from "console";
-import { resolve } from "path";
+import jwt from "jsonwebtoken";
 dotenv.config();
 const saltRounds = 10;
 
@@ -40,9 +40,19 @@ app.use(
   })
 );
 
+const corsOptions = {
+  origin: true,
+  optionsSuccessStatus: 200,
+  credentials: true
+}
+
 app.use(cors());
 
-const storage = multer.memoryStorage();
+const storage = multer.memoryStorage({
+  limits: {
+    fieldSize: 50 * 1024 * 1024,
+  },
+});
 const upload = multer({ storage: storage });
 
 const userSchema = {
@@ -62,17 +72,36 @@ const productSchema = {
 const User = new mongoose.model("User", userSchema);
 const Product = new mongoose.model("Product", productSchema);
 
+app.get("/logout", function (req, res) {
+  // Clear the access token from the response headers
+  res.clearCookie("access_token");
+  console.log("session destroyed");
+  res.send("success");
+});
+
+
 app.post("/login", async (req, res) => {
   try {
-    const foundUser = await User.findOne({ email: req.body.email }).exec();
+    const { email, password } = req.body; // Extract email and password directly
+
+    const foundUser = await User.findOne({ email: email }).exec(); // Use the extracted 'email'
 
     if (foundUser) {
-      const result = await bcrypt.compare(req.body.password, foundUser.password);
+      const result = await bcrypt.compare(password, foundUser.password); // Use the extracted 'password'
 
       if (result) {
+        const accessToken = jwt.sign(foundUser.toJSON(), process.env.ACCESS_TOKEN_SECRET);
+
+        res.cookie("access_token", accessToken, {
+          httpOnly: true,
+          secure: false, // Set to 'true' if using HTTPS
+          maxAge: 7 * 24 * 60 * 60 * 1000, // Expiration time in milliseconds (7 days in this example)
+        });
+
         res.status(200).json({
           message: "Login successful",
           role: foundUser.role,
+          accessToken: accessToken,
         });
       } else {
         res.status(401).json({ error: "Invalid credentials." });
@@ -102,9 +131,20 @@ app.post("/register", async (req, res) => {
 
     await newUser.save();
 
+    // Create and sign the access token
+    const accessToken = jwt.sign(newUser.toJSON(), process.env.ACCESS_TOKEN_SECRET);
+
+    // Set the access token as an HTTP cookie
+    res.cookie("access_token", accessToken, {
+      httpOnly: true,
+      secure: false, // Set to 'true' if using HTTPS
+      maxAge: 7 * 24 * 60 * 60 * 1000, // Expiration time in milliseconds (7 days in this example)
+    });
+
     res.status(201).json({
       message: "User added successfully",
       role: newUser.role,
+      accessToken: accessToken,
     });
   } catch (err) {
     res.status(500).json({ error: "Failed to register user." });
@@ -161,6 +201,56 @@ app.post("/products", upload.single("image"), async (req, res) => {
     res.status(201).json({ message: "Product added successfully." });
   } catch (err) {
     res.status(500).json({ error: "Failed to add product." });
+  }
+});
+
+
+// need to add the new image data and also update the data in the mongodb database
+app.put("/products", upload.single("image"), async (req, res) => {
+  try {
+    let product = await Product.findOne({ _id: req.body._id }).exec();
+    console.log(product);
+    if (product) {
+      const params = {
+        Bucket: bucketName,
+        Key: product.imageName
+      }
+      const deleteCommand = new DeleteObjectCommand(params);
+      await s3.send(deleteCommand);
+    }
+
+    const imageName = randomImageName();
+
+    const params = {
+      Bucket: bucketName,
+      Key: imageName,
+      Body: req.file.buffer,
+      ContentType: req.file.mimetype,
+    };
+
+    const putCommand = new PutObjectCommand(params);
+
+    await s3.send(putCommand);
+
+    const url = `https://${bucketName}.s3.${bucketRegion}.amazonaws.com/${imageName}`;
+    
+    product = await Product.findOneAndUpdate({_id: req.body._id}, {name: req.body.name, price: req.body.price, stock: req.body.stock, imageURL: url})
+
+    await product.save();
+    res.status(201).json({ message: "Product updated successfully." });    
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Failed to update product." });
+  }
+})
+
+app.delete("/products", async (req, res) => {
+  try {
+    const result = await Product.deleteOne({ name: req.body.name });
+    res.status(201).json({message: "Product deleted successfully!"});
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Failed to delete product." });
   }
 });
 
